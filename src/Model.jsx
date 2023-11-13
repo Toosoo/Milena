bb6bfc51f1b44a54f4007d85fbc7c20b534dbe1a
Toosoo <hello@iamtoso.com>
2023-11-13 14:45:49 +0200

    s

--- src/Model.jsx
+++ src/Model.jsx
@@ -5,15 +5,14 @@ import { useFrame } from "@react-three/fiber";
 
 export function Model(props) {
   const ref = useRef();
+  const ref2 = useRef();
   const { nodes, materials } = useGLTF("/female2.glb");
 
-  useFrame((state) => {
+  useFrame((state,delta) => {
     const sin = Math.sin(state.clock.elapsedTime  )
     const cos = Math.cos(state.clock.elapsedTime  )
-    // ref.current.layers[0].origin.set(cos / 2, 0, 0)
-    // ref.current.layers[1].origin.set(cos, sin, cos)
-    // ref.current.layers[2].origin.set(sin, cos, sin)
-    // ref.current.layers[3].origin.set(cos, sin, cos)
+    ref2.current.offset.x += delta * .01
+
   });
 
   return (
@@ -21,7 +20,7 @@ export function Model(props) {
       <mesh geometry={nodes.Object_3.geometry} castShadow>
        
          <LayerMaterial ref={ref} toneMapped={false}>
-           <Noise scale={30} colorA="black" colorB="black" colorC="black" colorD="white" type="curl" />
+           <Noise scale={30} colorA="black" colorB="black" colorC="black" colorD="white" type="curl" ref={ref2} />
         </LayerMaterial>   
       </mesh>
     </group>
